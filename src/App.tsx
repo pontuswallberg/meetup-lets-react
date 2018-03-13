50bf02e8d03c3073e6163a30bb0341c965d96cab
Pontus Wallberg <pontus.wallberg@gmail.com>
2018-03-13 20:25:59 +0100

    changed structure a bit

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,37 @@ interface Props {}
 
 interface State {
 	items: string[];
+	value: string;
 }
 
 class App extends React.Component<Props, State> {
 	state: State = {
 		items: [],
+		value: "",
 	};
-
-	additem = (event: React.FormEvent<HTMLFormElement>) => {
+	handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		this.setState({
+			value: event.target.value,
+		});
+	};
+	handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
-		const input = event.currentTarget.elements["add-item"];
-		const value = input.value;
-		if (value !== "") {
-			this.setState(
-				prevState => ({
-					items: prevState.items.concat(value),
-				}),
-				() => {
-					input.value = "";
-				}
-			);
+		if (this.state.value !== "") {
+			this.setState(prevState => ({
+				items: prevState.items.concat(prevState.value),
+				value: "",
+			}));
 		}
 	};
 
 	removeItem = (event: React.MouseEvent<HTMLButtonElement>) => {
-		event.preventDefault();
-		const parent = event.currentTarget.parentElement;
-		if (parent !== null) {
-			const index = parent.getAttribute("data-index");
-			if (index !== null) {
-				const items = this.state.items.slice();
-				items.splice(parseInt(index, 10), 1);
-				this.setState({ items: items });
-			}
-		}
+		const button = event.currentTarget;
+		const index = parseInt(button.getAttribute("data-index") || "", 10);
+		this.setState(prevState => {
+			const items = prevState.items.slice();
+			items.splice(index, 1);
+			return { items };
+		});
 	};
 
 	render() {
@@ -46,16 +43,21 @@ class App extends React.Component<Props, State> {
 			<div className="app">
 				<div className="app-content">
 					<h1>React Todo List</h1>
-					<form className="form" onSubmit={this.additem}>
+					<form className="form" onSubmit={this.handleSubmit}>
 						<label className="label" htmlFor="add-item">
 							Add item
 						</label>
-						<input className="input" name="add-item" id="add-item" type="text" />
+						<input
+							className="input"
+							id="add-item"
+							onChange={this.handleChange}
+							type="text"
+							value={this.state.value}
+						/>
 						<button className="button button-submit" type="submit">
 							Add
 						</button>
 					</form>
-
 					<h2>Todos</h2>
 					<TodoList items={this.state.items} removeItem={this.removeItem} />
 				</div>
@@ -66,24 +68,29 @@ class App extends React.Component<Props, State> {
 
 interface TodoListProps {
 	items: string[];
-	removeItem: (event: React.MouseEvent<HTMLButtonElement>) => void;
+	removeItem: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 function TodoList(props: TodoListProps) {
 	if (props.items.length === 0) {
 		return <p>No items</p>;
 	}
-
 	return (
 		<ul className="todo-list">
-			{props.items.map((item, index) => (
-				<li className="todo-list-item" data-index={index} key={index}>
-					{item}
-					<button className="button todo-list-remove-button" onClick={props.removeItem}>
-						Remove
-					</button>
-				</li>
-			))}
+			{props.items.map((item, index) => {
+				return (
+					<li className="todo-list-item" key={index}>
+						{item}
+						<button
+							className="button todo-list-remove-button"
+							data-index={index}
+							onClick={props.removeItem}
+						>
+							Remove
+						</button>
+					</li>
+				);
+			})}
 		</ul>
 	);
 }
